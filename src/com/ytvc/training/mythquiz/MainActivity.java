3abd1ebb9ac0bb59edaa7643076b418f91f51810
saronic <saronic@yeah.net>
2014-07-28 09:17:53 +0800

    第二章结束

--- src/com/ytvc/training/mythquiz/MainActivity.java
+++ src/com/ytvc/training/mythquiz/MainActivity.java
@@ -7,30 +7,56 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private Button mTrueButton;
 	private Button mFalseButton;
+	private TextView mQuestionTextView;
+	private Button mNextButton;
+	private TrueFalse[] mQuestionBank = new TrueFalse[] {
+			new TrueFalse(R.string.question_rain, false),
+			new TrueFalse(R.string.question_alcohol, false),
+			new TrueFalse(R.string.question_cat, true),
+			new TrueFalse(R.string.question_gas, false),
+			new TrueFalse(R.string.question_train, false) };
+
+	private int mCurrentIndex = 0;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mQuestionTextView = (TextView) findViewById(R.id.question_textview);
+		updateQuestion();
 		mTrueButton = (Button) findViewById(R.id.true_button);
 		mTrueButton.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+				checkAnswer(true);
+			}
+		});
+
+		mFalseButton = (Button) findViewById(R.id.false_button);
+		mFalseButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				checkAnswer(false);
 			}
 		});
 		
-		mFalseButton = (Button) findViewById(R.id.false_button);
-		mFalseButton.setOnClickListener(new OnClickListener() {
+		mNextButton = (Button) findViewById(R.id.next_button);
+		mNextButton.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+				
 			}
 		});
 	}
@@ -52,5 +78,22 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	private void updateQuestion() {
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
+	}
+	
+	private void checkAnswer(boolean userPressedTrue) {
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		int msgId = 0;
+		if (answerIsTrue == userPressedTrue) {
+			msgId = R.string.correct_toast;
+		} else {
+			msgId = R.string.incorrect_toast;
+		}
+		
+		Toast.makeText(this, msgId, Toast.LENGTH_SHORT).show();
 	}
 }
